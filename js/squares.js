ea0ef4c43ee5ad03f4eebbd9edfa3ff9a28632c4
Daniel Ajilore <mtme13@hotmail.com>
2018-08-16 12:47:58 +0100

    js refactor

--- js/squares.js
+++ js/squares.js
@@ -1,48 +1,49 @@
-var container = document.querySelector('.container'); // grab container div
-var button = document.getElementById('clearButton'); // grab the button
+var container = document.querySelector('.container'); 
+var button = document.getElementById('clearButton'); 
 
-function addDiv(divCount) {    
-    var setDimensions = ((600 / divCount) - 2).toFixed(2); // calculate grid cell dimensions
-    var numberOfGrids = Math.pow(divCount, 2); // calculate number of grid cells
+function addDiv(numberOfDivsToCreate) {    
+    var gridCellDimensions = ((600 / numberOfDivsToCreate) - 2).toFixed(2); 
+    var gridSize = Math.pow(numberOfDivsToCreate, 2); 
 
-    while (numberOfGrids > 0) {
-        var newDiv = document.createElement('div'); // create new div 	
-        container.appendChild(newDiv); // append as child of container div
-        newDiv.classList.add('grid'); // add '.grid' class to new div
-        newDiv.style.height = setDimensions + 'px'; // set grid cell height 
-        newDiv.style.width = setDimensions + 'px'; // set grid cell width 
-        newDiv.style.border = '1px solid black'; // adds border to grid cell        
-        numberOfGrids--; // iterate loop downwards
+    // create grid cells & add to container
+    while (gridSize > 0) {
+        var newDiv = document.createElement('div'); 	
+        container.appendChild(newDiv); 
+        newDiv.classList.add('grid'); 
+        newDiv.style.height = gridCellDimensions + 'px';  
+        newDiv.style.width = gridCellDimensions + 'px'; 
+        newDiv.style.border = '1px solid black';             
+        gridSize--; 
     }
-    var gridCells = document.querySelectorAll('.grid'); // grab all grid cells
-    gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor)); // adds mouse enter listener to each grid cell, change colour
-    console.log(setDimensions);
+
+    var gridCells = document.querySelectorAll('.grid'); 
+    gridCells.forEach(cell => cell.addEventListener('mouseenter', changeColor)); 
+    console.log(gridCellDimensions);
     console.log('Grid has been created!!')
 }
 
 // CHANGE CELL COLOR 
 function changeColor() {
-    this.style.backgroundColor = '#ff9999'; // change entered grid cell to red
+    this.style.backgroundColor = '#ff9999'; 
 }
 
 // CLEAR GRID & PROMPT NEW GRID
-function clear() {
-    var gridCells = document.querySelectorAll('.grid'); // grab all grid cells
-    gridCells.forEach(cells => cells.style.backgroundColor = '#1C86EE'); // change all grid cells to blue	
-    var reqGridSize = prompt('How many squares per side?'); // user supplied value for new number of grid cells
+function clear() {    	
+    var reqGridSize = prompt('How many squares per side?'); 
 
-    if (reqGridSize >= 1 && reqGridSize <= 100) { // check value is 1-100
+    if (reqGridSize >= 1 && reqGridSize <= 100) { 
         while (container.hasChildNodes()) {
             container.removeChild(container.lastChild); // removes all child nodes from container div
         }
-        addDiv(reqGridSize); // create new grid using user supplied value
+        addDiv(reqGridSize); // create new grid 
     } else {
-        return ('Choose a number between 1-100'); // if value invalid, suggest valid value
+        alert ('Choose a number between 1-100'); 
+        clear();
     }
 
 }
 
-button.addEventListener('click', clear); // add click listener to button, runs clear() function
+button.addEventListener('click', clear); 
 window.onload = addDiv(16); // on page load, create a 16 x 16 grid
 
 // JUNE 2017 VERSION
