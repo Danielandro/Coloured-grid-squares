702ac39375314bd284bb06fc0900bc6b97c35a76
Daniel Ajilore <mtme13@hotmail.com>
2018-08-16 13:23:58 +0100

    remove original js attempts

--- js/squares.js
+++ js/squares.js
@@ -5,7 +5,7 @@ function addDiv(numberOfDivsToCreate) {
     var gridCellDimensions = ((600 / numberOfDivsToCreate) - 2).toFixed(2); 
     var gridSize = Math.pow(numberOfDivsToCreate, 2); 
 
-    // create grid cells & add to container
+    // create grid squares & add to container
     while (gridSize > 0) {
         var newDiv = document.createElement('div'); 	
         container.appendChild(newDiv); 
@@ -22,89 +22,26 @@ function addDiv(numberOfDivsToCreate) {
     console.log('Grid has been created!!')
 }
 
-// CHANGE CELL COLOR 
+// change grid square color to red
 function changeColor() {
     this.style.backgroundColor = '#ff9999'; 
 }
 
-// CLEAR GRID & PROMPT NEW GRID
+// clear grid + prompt for new grid size
 function clear() {    	
     var reqGridSize = prompt('How many squares per side?'); 
 
     if (reqGridSize >= 1 && reqGridSize <= 100) { 
         while (container.hasChildNodes()) {
-            container.removeChild(container.lastChild); // removes all child nodes from container div
+            container.removeChild(container.lastChild); // removes all grid squares
         }
         addDiv(reqGridSize); // create new grid 
     } else {
         alert ('Choose a number between 1-100'); 
         clear();
     }
-
 }
 
 button.addEventListener('click', clear); 
 window.onload = addDiv(16); // on page load, create a 16 x 16 grid
 
-// JUNE 2017 VERSION
-/*$(document).ready(function(){
-
-$div = $('<div></div>')
-addDiv = $div;
-
-  function extraDiv(){
-      $('.container').append(addDiv);
-};
-
-divCount = ('div').length;
-if (divCount < 17){
-  extraDiv();
-}
-
-
-//place loop here
-
-
-});
-
-/*divCount = ('div').length;
-if (divCount < 17){
-  div();
-}*/
-
-//var divCol = ('<div class = "column"><div>');
-
-
-// $(document).ready(function(){
-//   divRow = $('<div class="row" height="50px" width="50px"><div>');//  $('.container').append(divRow);
-// });
-
-// alert($('div').length);
-
-// var divCount = 17; // No of Div's to create
-
-
-//NOV 2017 VERSION - RECURSION ATTEMPT
-// need divCount = Math.pow(reqDivSize, 2)
-
-// function addDiv(divCount) {
-//     var container = document.querySelector('.container'); // select container div        
-//     var newDiv = document.createElement('div'); // creates a new div
-
-
-
-//     if (divCount <= 0) {
-//         return console.log('Divs created!!!'); // BASE CASE
-//     } else {
-
-//         container.appendChild(newDiv); // adds new div (as a child of) container div 
-//         newDiv.classList.add('grid'); // adds '.grid' class 
-
-//         newDiv.style.height = '35.5px'; // set height 
-//         newDiv.style.width = '35.5px'; // set width 
-//         newDiv.style.border = '1px solid black'; // adds border         
-
-//         return addDiv(divCount - 1); // RECURSIVE CASE
-//     }
-//     // functions here will not run
-// }
